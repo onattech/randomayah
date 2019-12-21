207d6c8fb1b3def5b01fb08e61996110d915e6ca
lokutech <onatim@protonmail.com>
2019-12-21 19:35:57 +0300

    Updates

---
+++ precache-manifest.4ae9c28e10be5ceb245eb80ee1b8bd90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b1d4557b3222a078ff0902bd54bb5f",
+    "url": "/wallabyjs/quokka-react-sample/index.html"
+  },
+  {
+    "revision": "0d319760f4c31412df1a",
+    "url": "/wallabyjs/quokka-react-sample/static/css/main.4b3884d2.chunk.css"
+  },
+  {
+    "revision": "fc7d34a22687d0639499",
+    "url": "/wallabyjs/quokka-react-sample/static/js/2.39fbd4e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wallabyjs/quokka-react-sample/static/js/2.39fbd4e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d319760f4c31412df1a",
+    "url": "/wallabyjs/quokka-react-sample/static/js/main.ea07dd69.chunk.js"
+  },
+  {
+    "revision": "1265a70fc04b69574bf9",
+    "url": "/wallabyjs/quokka-react-sample/static/js/runtime-main.7027e5fd.js"
+  },
+  {
+    "revision": "d45f5c63cd51c45de90ed8d823fe9610",
+    "url": "/wallabyjs/quokka-react-sample/static/media/twitter.d45f5c63.svg"
+  }
+]);
