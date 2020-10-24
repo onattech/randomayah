638e64e547cf516cfa705e38ac7b60a9459c5fdc
Loku <onatim@protonmail.com>
2020-10-24 11:25:16 +0300

    some clean up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import './App.css'
-import twitter from './twitter.svg'
 import ayahobject from './ayahobject.json'
 
 class App extends React.Component {
   state = {
     ayah: '',
-    uthmani: '',
     surah: '',
     color: '',
     loading: false,
-    ayahNumber: 0
+    ayahNumber: 0,
   }
 
   handleClick = () => {
@@ -27,20 +25,19 @@ class App extends React.Component {
     this.setState({
       loading: false,
       ayahNumber: ayahobject[currentAyah].numberInSurahNativeReverse,
-      ayah: ayahobject[currentAyah].text,
-      uthmani: ayahobject[currentAyah].uthmaniText,
+      ayah: ayahobject[currentAyah].uthmaniText,
       surah: ayahobject[currentAyah].surah.name,
-      color: randomcolor()
+      color: randomcolor(),
     })
   }
 
   render() {
     document.body.style.backgroundColor = this.state.color
-    let { surah, color, ayahNumber, loading, uthmani } = this.state
+    let { surah, color, ayahNumber, loading, ayah } = this.state
     return (
       <div>
         <Quote
-          ayah={uthmani}
+          ayah={ayah}
           surah={surah}
           color={color}
           handleClick={this.handleClick}
@@ -52,57 +49,46 @@ class App extends React.Component {
   }
 }
 
-const Quote = props => {
+const Quote = (props) => {
   let style = {
     color: props.color,
-    display: 'inline'
+    display: 'inline',
   }
   if (props.loading) style.display = 'none'
   console.log(props.ayahNumber, 'ayah number')
 
   return (
-    <div id='quote-box'>
-      {props.loading && <p id='loading'>...جار التحميل</p>}
+    <div id="quote-box">
+      {props.loading && <p id="loading">...جار التحميل</p>}
       <p
         style={{ color: props.color, display: props.loading && 'none' }}
-        id='text'
+        id="text"
       >
         {props.ayah} {props.ayahNumber}
       </p>
       <p
         style={{ color: props.color, display: props.loading && 'none' }}
-        id='surah'
+        id="surah"
       >
         {props.surah}
       </p>
       <button
-        id='button'
+        id="button"
         style={{ backgroundColor: props.color }}
         onClick={props.handleClick}
-        className='new-quote'
+        className="new-quote"
       >
         آية جديدة
       </button>
-      <div id='twitterbox'>
-        <a
-          href={`https://twitter.com/intent/tweet?hashtags=Quran&text=${props.ayah}`}
-        >
-          <img
-            src={twitter}
-            style={{ backgroundColor: props.color }}
-            alt={'tweet bird'}
-          />
-        </a>
-      </div>
     </div>
   )
 }
 
 function randomNumber() {
-  const firstAyah = 1
-  const lastAyah = 6236
-  const diff = lastAyah - firstAyah + 1 // added 1 because the range doesn't include the last Ayah
-  return Math.floor(Math.random() * diff + firstAyah)
+  const startingAyah = 1
+  const endingAyah = 6236
+  const diff = endingAyah - startingAyah + 1 // added 1 because the range doesn't include the last Ayah
+  return Math.floor(Math.random() * diff + startingAyah)
 }
 
 function randomcolor() {
@@ -127,47 +113,12 @@ function randomcolor() {
     18: '#39BF94',
     19: '#FC6CB2',
     20: '#C13A50',
-    21: '#342224'
+    21: '#342224',
   }
-  const firstAyah = 1
+
   const diff = Object.keys(colors).length
-  let selectedColor = Math.floor(Math.random() * diff + firstAyah)
+  let selectedColor = Math.floor(Math.random() * diff + 1)
   return colors[selectedColor]
 }
 
 export default App
-
-// function convertToInd(num) {
-//   const dict = {
-//     0: '٠',
-//     1: '١',
-//     2: '٢',
-//     3: '٣',
-//     4: '٤',
-//     5: '٥',
-//     6: '٦',
-//     7: '٧',
-//     8: '٨',
-//     9: '٩'
-//   }
-//   let x = String(num)
-//     .split('')
-//     .map(a => dict[a])
-//     .join('')
-//   return x
-// }
-
-// function reverseForKFGQPC(num) {
-//   if (num.length < 3) {
-//     return String(num)
-//     .split('')
-//     .reverse()
-//     .join('')
-//   } else {
-//     let array = String(num).split('')
-//     return String(num)
-//     .split('')
-//     .reverse()
-//     .join('')
-//   }
-// }
